defebe0b7b1dfdab6185ec32e8cbc318693c2637
Manou-02 <mrakotoarivelo97@gmail.com>
2021-10-31 15:37:07 +0100

    Header changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const App = () => {
     return (
         <div className="">
-            <h1>Hello word!</h1>
+            <h1>Hello Manou</h1>
         </div>
     )
 }
